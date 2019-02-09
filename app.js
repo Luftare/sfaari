2b7e191547700bfccbd7360d01cc93a2243db08a
luftare <ilmari.koskinen1@gmail.com>
2019-02-09 18:09:46 +0200

    Remove duplicate middleware

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
-const isTestEnvironment = process.env.NODE_ENV === 'test';
-const envFilePath = isTestEnvironment ? './.test-env' : './.env';
+const isTestEnvironment = process.env.NODE_ENV === "test";
+const envFilePath = isTestEnvironment ? "./.test-env" : "./.env";
 
-require('dotenv').config({ path: envFilePath });
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const {
-  hasValidToken,
-  hasRoles,
-  ownUserId
-} = require('./middleware/verifyToken');
-const { admin, login, users } = require('./handlers');
-const dataAccessObject = require('./dataAccessObject');
+require("dotenv").config({ path: envFilePath });
+const express = require("express");
 const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser");
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+const { hasValidToken, hasRoles, ownUserId } = require("./middleware/verifyToken");
+const { admin, login, users } = require("./handlers");
+const dataAccessObject = require("./dataAccessObject");
+
 app.use(bodyParser.json());
-app.post('/login', login.post);
-app.get('/admin', hasValidToken, hasRoles(['admin']), admin.get);
-app.get('/users', users.getAll);
-app.post('/users', users.post);
-app.get('/users/:userId', users.get);
-app.put('/users/:userId/username', hasValidToken, ownUserId, users.putUsername);
-app.put('/users/:userId/password', hasValidToken, ownUserId, users.putPassword);
-app.use('/', express.static(__dirname + '/client/dist'));
+
+router.post("/login", login.post);
+router.get("/admin", hasValidToken, hasRoles(["admin"]), admin.get);
+router.get("/users", users.getAll);
+router.post("/users", users.post);
+router.get("/users/:userId", users.get);
+router.put("/users/:userId/username", hasValidToken, ownUserId, users.putUsername);
+router.put("/users/:userId/password", hasValidToken, ownUserId, users.putPassword);
+
+app.use("/", express.static(__dirname + "/client/dist"));
+app.use("/", router);
 
 async function init() {
   return await dataAccessObject.init();
