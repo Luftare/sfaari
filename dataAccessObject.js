efa3d1a2017808018354d31e34222d16672d431b
Ilmari <ilmari.koskinen1@gmail.com>
2019-02-11 12:45:26 +0200

    Refactor DAO

--- dataAccessObject.js
+++ dataAccessObject.js
@@ -18,15 +18,10 @@ module.exports = {
       console.log(err);
     }
 
-    const adminUserExists = await this.userWithUsernameExists(
-      process.env.ADMIN_USERNAME
-    );
+    const adminUserExists = await this.userWithUsernameExists(process.env.ADMIN_USERNAME);
 
     if (!adminUserExists) {
-      await this.addUser(
-        process.env.ADMIN_USERNAME,
-        process.env.ADMIN_PASSWORD
-      );
+      await this.addUser(process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD);
     }
 
     if (isTestEnvironment) {
@@ -75,10 +70,7 @@ module.exports = {
       const userExists = user && user.username;
       if (!userExists) return false;
 
-      const correctPasswordProvided = await bcrypt.compare(
-        password,
-        user.password
-      );
+      const correctPasswordProvided = await bcrypt.compare(password, user.password);
 
       return correctPasswordProvided;
     } catch (err) {
@@ -87,10 +79,8 @@ module.exports = {
     }
   },
 
-  async getRole(name) {
-    const role = await this.db.get(`SELECT * FROM Role WHERE name = (?)`, [
-      name
-    ]);
+  async getRoleByName(name) {
+    const role = await this.db.get(`SELECT * FROM Role WHERE name = (?)`, [name]);
 
     return role;
   },
@@ -104,15 +94,10 @@ module.exports = {
     return rolesObject.map(({ name }) => name);
   },
 
-  async getUserById(id) {
-    const user = await this.db.get(`SELECT * FROM User WHERE id = (?)`, [id]);
-
-    const userNotFound = !user;
-
-    if (userNotFound) {
-      return null;
+  async getEnrichedUser(user) {
+    if (!user) {
+      return new Error('User not found.');
     }
-
     const roles = await this.getUserRolesById(user.id);
 
     return {
@@ -121,50 +106,35 @@ module.exports = {
     };
   },
 
+  async getUserById(id) {
+    const user = await this.db.get(`SELECT * FROM User WHERE id = (?)`, [id]);
+    return await this.getEnrichedUser(user);
+  },
+
   async getUserByName(username) {
-    const user = await this.db.get(`SELECT * FROM User WHERE username = (?)`, [
-      username
-    ]);
-
-    const userNotFound = !user;
-
-    if (userNotFound) {
-      return null;
-    }
-
-    const roles = await this.getUserRolesById(user.id);
-
-    return {
-      ...user,
-      roles
-    };
+    const user = await this.db.get(`SELECT * FROM User WHERE username = (?)`, [username]);
+    return await this.getEnrichedUser(user);
   },
 
   async updateUserUsername(id, username) {
     const user = await this.getUserById(id);
 
     if (!user) {
-      throw new Error('User not found.');
+      return new Error('User not found.');
     }
 
-    return await this.db.run('UPDATE User SET username = (?) WHERE id = (?)', [
-      username,
-      id
-    ]);
+    return await this.db.run('UPDATE User SET username = (?) WHERE id = (?)', [username, id]);
   },
 
   async updateUserPassword(id, password) {
     const user = await this.getUserById(id);
 
     if (!user) {
-      throw new Error('User not found.');
+      return new Error('User not found.');
     }
 
     const hashedPassword = await bcrypt.hash(password, encryptionSaltRounds);
-    return await this.db.run('UPDATE User SET password = (?) WHERE id = (?)', [
-      hashedPassword,
-      id
-    ]);
+    return await this.db.run('UPDATE User SET password = (?) WHERE id = (?)', [hashedPassword, id]);
   },
 
   async getAllUsers() {
@@ -192,28 +162,21 @@ module.exports = {
       const userExists = await this.userWithUsernameExists(username);
 
       if (userExists) {
-        throw new Error(`User with username of "${username}" already exists!`);
+        return new Error(`User with username of "${username}" already exists!`);
       }
 
       const hashedPassword = await bcrypt.hash(password, encryptionSaltRounds);
 
-      await db.run('INSERT INTO User (username, password) VALUES (?, ?)', [
-        username,
-        hashedPassword
-      ]);
+      await db.run('INSERT INTO User (username, password) VALUES (?, ?)', [username, hashedPassword]);
 
       const adminCredentialsProvided =
-        username === process.env.ADMIN_USERNAME &&
-        password === process.env.ADMIN_PASSWORD;
+        username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD;
 
       if (adminCredentialsProvided) {
         const user = await this.getUserByName(username);
-        const role = await this.getRole('admin');
+        const role = await this.getRoleByName('admin');
 
-        return await db.run(
-          'INSERT INTO UserRole (userId, roleId) VALUES (?, ?)',
-          [user.id, role.id]
-        );
+        return await db.run('INSERT INTO UserRole (userId, roleId) VALUES (?, ?)', [user.id, role.id]);
       } else {
         return;
       }
