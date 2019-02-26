2fd021f9f45e1e8ef0c5d9902c8a58fc5fa5e66d
Ilmari <ilmari.koskinen1@gmail.com>
2019-02-26 14:19:15 +0200

    Add authenticated routing at client

--- client/src/router.ts
+++ client/src/router.ts
@@ -4,7 +4,7 @@ import Home from './views/Home.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -21,7 +21,21 @@ export default new Router({
     {
       path: '/profile',
       name: 'profile',
-      component: () => import('./views/Profile.vue')
+      component: () => import('./views/Profile.vue'),
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = localStorage.getItem('jwt-token');
+  const redirectToLogin = requiresAuth && !isAuthenticated;
+  const nextPath = redirectToLogin ? '/login' : {};
+
+  next(nextPath);
+});
+
+export default router;
